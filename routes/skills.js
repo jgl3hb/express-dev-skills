6535fa02e1263e9fa8922723a025081f6fc8b022
jgl3hb <lewis.jeffreyg@gmail.com>
2022-02-27 12:14:48 -0500

    routes

--- routes/skills.js
+++ routes/skills.js
@@ -1,10 +1,17 @@
 import { Router } from 'express'
+import * as skillsCtrl from "../controllers/skills.js"
 const router = Router()
 
 /* GET users listing. */
-router.get('/', function(req, res) {
-  res.send('respond with a resource')
-})
+// GET - localhost:3000/todos
+router.get('/', skillsCtrl.index)
+// GET - localhost:3000/todos/new
+router.get("/new", skillsCtrl.new)
+// GET - localhost:3000/todos/:id
+router.get("/:id", skillsCtrl.show)
+
+// POST - localhost:3000/todos
+router.post("/", skillsCtrl.create)
 
 export {
   router
